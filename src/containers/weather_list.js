e7caab815ca2bdf2bc50000d70d2f167cb21eab5
petrios888 <appeter22@yahoo.com>
2017-04-10 19:38:36 -0400

    Delta

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,17 +1,41 @@
 import React , { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+
+
+class WeatherList extends Component {
+  renderWeather (cityData) {
+    const name = cityData.city.name;
+
+    return(
+        <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+
   render (){
     return (
-      <table classname= "table table-hover">
+      <table className= "table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperture</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
           <tbody>
+            {this.props.weather.map(this.renderWeather)}
           </tbody>
       </table>
     );
   }
 }
+
+
+function mapStateToProps  ({weather}) {
+  return { weather };
+}
+
+export default connect (mapStateToProps)( WeatherList);
