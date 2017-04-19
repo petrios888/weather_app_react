2d4de033d8b3361d98f0e620b9d0ebce965563b3
petrios888 <appeter22@yahoo.com>
2017-04-18 20:59:36 -0400

    theta

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,7 @@
 import React , { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
-
+import GoogleMap from '../components/google_map';
 
 
 class WeatherList extends Component {
@@ -10,14 +10,17 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather =>  weather.main.temp);
     const pressures= cityData.list.map(weather => weather.main.pressure);
     const humidities= cityData.list.map(weather => weather.main.humidity);
+    const {lon ,lat} = cityData.city.coord;
+
+
 
 
     return(
-        <tr key={name}>
-        <td>{name}</td>
-        <td><Chart data={temps} color="blue"/></td>
-        <td><Chart data={pressures} color="orange"/></td>
-        <td><Chart data={humidities} color="yellow"/></td>
+      <tr key={name}>
+        <td><GoogleMap lon={lon} lat={lat} /> </td>
+        <td><Chart data={temps} color="blue" units="K"/></td>
+        <td><Chart data={pressures} color="orange"units="hPA"/></td>
+        <td><Chart data={humidities} color="yellow" units="%"/></td>
       </tr>
     );
   }
